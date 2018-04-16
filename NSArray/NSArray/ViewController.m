6b600c6b2e0bbffe3fa906be9d14c9c48481d903
paulkoka <pavelkoka@gmail.com>
2018-04-16 14:48:07 +0300

    tesk moved to method

--- NSArray/NSArray/ViewController.m
+++ NSArray/NSArray/ViewController.m
@@ -15,63 +15,69 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad]; NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    [super viewDidLoad];
+    [self doTask];
     
-    
-#pragma mark NSArray
+}
+
+- (void) doTask{
     //    Please, find your tasks on NSArray and NSMutableArray bellow:
     //
     //    Create NSArray, containing several strings, using literal declaration.
+        NSArray *usingStringLiterals = @[@"first", @"second", @"third", @"fourth"];
     
-    NSArray *usingStringLiterals = @[@"first", @"second", @"third", @"fourth"];
+    
     //    Create mutable array from piviously created NSArray.
+    NSMutableArray *mutableFromNSArray = [NSMutableArray arrayWithArray:usingStringLiterals];
     
-    NSMutableArray *mutableFromNSArray= [NSMutableArray arrayWithArray:usingStringLiterals];
     
     //    Create an empty array and obtain its first and last element in a safe way.
-    NSArray *emptyArray = [[[NSArray alloc] init] autorelease];
+    NSArray *emptyArray = [NSArray array];
     [emptyArray firstObject];
     [emptyArray lastObject];
     
+    
     //    Create NSArray, containing strings from 1 to 20:
     [mutableFromNSArray removeAllObjects];
-    for (int i=1; i<=20; i++) {
-        [mutableFromNSArray addObject:[NSString stringWithFormat:@"%d", i ]];
+    
+    for (int i = 1; i <= 20; i++) {
+        [mutableFromNSArray addObject:[NSString stringWithFormat:@"%d", i]];
     }
     
-    NSArray *arrayOfStringsFromOneTillTwenty = [ NSArray arrayWithArray:
-                                                mutableFromNSArray ];
+    NSArray *arrayOfStringsFromOneTillTwenty = [NSArray arrayWithArray:                                                                                                    mutableFromNSArray];
+    
     
     //    Get its shallow copy and real deep copy.
     //shallow copy
-    NSArray *shallowCopy = [NSMutableArray arrayWithArray:arrayOfStringsFromOneTillTwenty];
-    // real deep copy
-    NSArray *deepCopy = [NSKeyedUnarchiver unarchiveObjectWithData:[NSKeyedArchiver
-                                                                    
-                        archivedDataWithRootObject:arrayOfStringsFromOneTillTwenty]];
+    NSArray *shallowCopy = [NSArray arrayWithArray:arrayOfStringsFromOneTillTwenty];
     
+    
+    // real deep copy
+    NSArray *deepCopy = [NSKeyedUnarchiver unarchiveObjectWithData:[NSKeyedArchiver                                                                         archivedDataWithRootObject:arrayOfStringsFromOneTillTwenty]];
     
     
     //    Iterate over array and obtain item at index 13. Print an item.
-    for (int i=0; i<deepCopy.count; i++) {
-        if (i==13) {
+    for (int i = 0; i < deepCopy.count; i++) {
+        if (i == 13) {
             NSLog(@"%@", deepCopy[i]);
             break;
         }
     }
     //    Make array mutable. Add two new entries to the end of the array, add an entry to the
-    //beginning of the array. 
-    arrayOfStringsFromOneTillTwenty = [arrayOfStringsFromOneTillTwenty mutableCopy];
+    //beginning of the array.
+    NSMutableArray *mutableArrayOfStringsFromOneTillTwenty = [arrayOfStringsFromOneTillTwenty mutableCopy];
+    
     
     //Add two new entries to the end of the array
-    [(NSMutableArray*) arrayOfStringsFromOneTillTwenty addObject:@"21"];
-    [(NSMutableArray*) arrayOfStringsFromOneTillTwenty addObject:@"22"];
-    [(NSMutableArray*) arrayOfStringsFromOneTillTwenty insertObject:@"0" atIndex:0];
+    [mutableArrayOfStringsFromOneTillTwenty addObject:@"21"];
+    [mutableArrayOfStringsFromOneTillTwenty addObject:@"22"];
+    [mutableArrayOfStringsFromOneTillTwenty insertObject:@"0" atIndex:0];
+    
     
     // Iterate over an array and remove item at index 5.
-    for (int i= 0; i<arrayOfStringsFromOneTillTwenty.count; i++) {
-        if(i==5){
-            [(NSMutableArray*)arrayOfStringsFromOneTillTwenty  removeObjectAtIndex:i];
+    for (int i = 0; i < mutableArrayOfStringsFromOneTillTwenty.count; i++) {
+        if(i == 5){
+            [mutableArrayOfStringsFromOneTillTwenty  removeObjectAtIndex:i];
             break;
         }
     }
@@ -81,8 +87,8 @@
     //
     NSMutableArray *mixedNumbers = [NSMutableArray arrayWithCapacity:10];
     
-    for (int i=0; i<10; i++ ) {
-        mixedNumbers[i]=[NSNumber numberWithInteger:arc4random()%100];
+    for (int i = 0; i < 10; i++ ) {
+        mixedNumbers[i] = [NSNumber numberWithInteger:arc4random()%100];
     }
     
     //    Sort it in an ascending
@@ -91,14 +97,10 @@
     //    Sort it in an descending
     NSArray *sortedDescending = [mixedNumbers sortedArrayUsingComparator:
                                  ^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
-                                     
-                                     return [obj2 compare:obj1];
-                                 }];
-    [arrayOfStringsFromOneTillTwenty release];
-    [pool release];
-    // Do any additional setup after loading the view, typically from a nib.
+                                return [obj2 compare:obj1];
+                                }];
+    [mutableArrayOfStringsFromOneTillTwenty release];
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
